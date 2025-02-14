2d5707c13167e62d9c4091e4e7ac3ef4d08384c9
Gevor <gev.srg.044@gmail.com>
2025-02-15 00:27:30 +0400

    final

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -5,7 +5,6 @@ from loader import bot
 @bot.message_handler(commands=["start"])
 def bot_start(message: Message):
     welcome_text = (
-        "Manen debila\n"
         "👋 <b>Привет! Я футбольный бот!</b>\n\n"
         "⚽ Я могу искать игроков, их статистику, команды и лиги.\n"
     )
